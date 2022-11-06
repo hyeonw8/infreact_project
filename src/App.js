f3032d71d939f87250edc82cf1d680b861adf977
hyeonw8 <whgusrud223@gmail.com>
2022-11-07 01:14:50 +0900

    commit2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import AboveGame from "./component/AboveGame";
+import Game from "./component/Game";
+import Header from "./component/Header";
 
 export default function App() {
   return (
-    <div className="App">adf</div>;
+    <div className="container">
+      <Header />
+      <AboveGame />
+      <Game />
+    </div>
+  );
 }
     
---
+++ src/component/Game.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function Game() {
+    <div className="game-container">
+        <div className="grid-container">
+            <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+            </div>
+            <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+            </div>
+            <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+            </div>
+            <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+            </div>
+        </div>
+        <div class="tile-container">
+            <div class="tile tile-2 tile-position-3-1">
+                <div class="tile-inner">2</div>
+            </div>
+            <div class="tile tile-2 tile-position-3-1">
+                <div class="tile-inner">2</div>
+            </div>
+            <div class="tile tile-4 tile-position-3-1 tile-merged">
+                <div class="tile-inner">4</div>
+            </div>
+            <div class="tile tile-4 tile-position-4-1">
+                <div class="tile-inner">4</div>
+            </div>
+            <div class="tile tile-4 tile-position-4-1">
+                <div class="tile-inner">4</div>
+            </div>
+            <div class="tile tile-8 tile-position-4-1 tile-merged">
+                <div class="tile-inner">8</div>
+            </div>
+            <div class="tile tile-2 tile-position-4-4 tile-new">
+                <div class="tile-inner">2</div>
+            </div>
+        </div>
+    </div>
+};
