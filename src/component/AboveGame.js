c4b480bc642d5aa013b831b5fb1570c2fa34dfe6
hyeonw8 <whgusrud223@gmail.com>
2022-11-14 16:36:23 +0900

    new commit!

--- src/component/AboveGame.js
+++ src/component/AboveGame.js
@@ -6,7 +6,7 @@ export default function AboveGame() {
            <p className='game-intro'>
             Join the numbers and get to the <strong>2048 title!!</strong>
            </p>
-           <a href='/' className='restart-button'>
+           <a href="/" className="restart-button">
             New Game
            </a>
         </div>
